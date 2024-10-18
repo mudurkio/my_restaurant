ff174304195a588db2466f5d4ae5600fea62b6d1
Ann Chemutai <Ann@ilabafrica19.strathmore.local>
2024-10-18 12:08:07 +0300

    adding sanctum

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return $categories;
     }
 
     /**
